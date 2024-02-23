3c3d77b32010628d7b2d0a9d35cad3e3b880924c
Christophe Joanny <christophe.joanny@gmail.com>
2024-02-23 16:04:55 +0100

    FirstProjectInCpp/main.cpp

---
+++ FirstProjectInCpp/FirstProjectInCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FirstProjectInCpp
+//
+//  Created by Christophe Joanny on 15/09/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
